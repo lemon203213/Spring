c47d3dcf9a4b55a1556f0978fc4c2605e0f3d902
lemon203213 <lemon203213@gmail.com>
2022-07-07 00:47:46 +0900

    필요없는 부분 삭제

--- Week03springhwk/src/main/java/com/example/week03springhwk/domain/PostRepository.java
+++ Week03springhwk/src/main/java/com/example/week03springhwk/domain/PostRepository.java
@@ -5,8 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 
+
 public interface PostRepository extends JpaRepository<Post, Long> {
     List<Post> findAllByOrderByModifiedAtDesc();
+
 
 
 }//ok
